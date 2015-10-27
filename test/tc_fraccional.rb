4f4793f1b996b975f3a8b3f910a7300a1082e9c4
David Redondo Durand <alu0100851700@ull.edu.es>
2015-10-27 23:40:27 +0000

    Creado test resta

--- test/tc_fraccional.rb
+++ test/tc_fraccional.rb
@@ -10,4 +10,8 @@ class TestFraccional < Test::Unit::TestCase
 	def test_suma
 		assert_equal("25/50", (Fraccional.new(1,5) + Fraccional.new(3,10)).to_s)
 	end
+
+	def test_resta
+		assert_equal("-5/50", (Fraccional.new(1,5) - Fraccional.new(3,10)).to_s)
+	end
 end
